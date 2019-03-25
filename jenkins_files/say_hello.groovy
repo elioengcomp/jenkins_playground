5c543543207611b9c4b83fd9c75d3ab1456b4512
Elio Marcolino <eliom@jfrog.com>
2019-03-25 15:24:43 -0700

    Update say_hello.groovy

--- jenkins_files/say_hello.groovy
+++ jenkins_files/say_hello.groovy
@@ -1,6 +1,6 @@
 node {
     stage('Say hello') {
         sh "pwd"
-        sh "python -u jenkins_files/scripts/hello.py $MYARG"
+        sh "python -u ./jenkins_files/scripts/hello.py $MYARG"
     }
 }
