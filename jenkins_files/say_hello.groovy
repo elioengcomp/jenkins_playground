75018ab16d1b1e8468d81a191aa4a0c3c8c77ee1
Elio Marcolino <eliom@jfrog.com>
2019-03-25 15:27:57 -0700

    Update say_hello.groovy

--- jenkins_files/say_hello.groovy
+++ jenkins_files/say_hello.groovy
@@ -1,6 +1,12 @@
 node {
+    
+    stage('Clone repo') {
+        git branch: 'master', url: 'https://github.com/elioengcomp/jenkins_playground.git'   
+    }
+    
     stage('Say hello') {
         sh "pwd"
+        sh "ls -la"
         sh "python -u ./jenkins_files/scripts/hello.py $MYARG"
     }
 }
